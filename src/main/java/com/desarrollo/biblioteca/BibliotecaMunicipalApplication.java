07cf21c53d4720d06f909212f0274167d5fe53e0
Juan Alvarez <Pyrosea98@gmail.com>
2020-06-12 03:09:48 -0500

    First commit for real

---
+++ src/main/java/com/desarrollo/biblioteca/BibliotecaMunicipalApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaMunicipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaMunicipalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desarrollo/biblioteca/BibliotecaMunicipalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaMunicipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
